c74e50a7b03c30c30ac7b189d142b78dc6ba36d9
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-10-01 04:19:03 +0000

    Swiper導入

---
+++ SlideShow-Lesson/script.js
@@ -0,0 +1,13 @@
+/* global Swiper*/
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* global $*/
 $(function(){
-  $('.box1').on('click', function(){
-    $(this).slideUp();
+  $('button').on('click', function(){
+    $('ul').children().css('color', 'red');
   });
 });
