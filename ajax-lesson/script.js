050f3c245e45d53508ed9ebf280404f65258a9ae
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-10-02 02:11:23 +0000

    Ajax　WebAPI学習

---
+++ ajax-lesson/script.js
@@ -0,0 +1,21 @@
+/* global $*/
+const API_KEY = "42627aed49ff9530b069f0d6bd6ec648";
+$(function(){
+  $('#btn').on('click', function(){
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function(data){
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('#img').attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('#img').attr("alt", data.weather[0].main);
+    }).fail(function(data){
+      alert('通信に失敗しました。')
+    })
+  });
+});
